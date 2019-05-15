543859ba8a76baca741249b2c27175fb2618db5b
x_false <514286291@qq.com>
2019-05-15 23:16:28 +0800

    尝试taro ui

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,6 +1,6 @@
 import Taro, { Component } from "@tarojs/taro"
 import { View, Swiper, SwiperItem } from "@tarojs/components"
-import { AtNoticebar, AtGrid, } from "taro-ui"
+import { AtNoticebar, AtGrid, AtAccordion, AtList, AtListItem } from "taro-ui"
 import "./index.css"
 import A from "./imgs/a.jpg"
 import B from "./imgs/b.jpg"
@@ -10,16 +10,26 @@ export default class Index extends Component {
   config = {
     navigationBarTitleText: "首页"
   }
+  constructor () {
+    super(...arguments)
+    this.state = {
+      open: false,
+    }
+  }
+  handleClickAAA (value) {
+    this.setState({
+      open: value
+    })
+  }
+  componentWillMount() {}
 
-  componentWillMount() { }
+  componentDidMount() {}
 
-  componentDidMount() { }
+  componentWillUnmount() {}
 
-  componentWillUnmount() { }
+  componentDidShow() {}
 
-  componentDidShow() { }
-
-  componentDidHide() { }
+  componentDidHide() {}
 
   handleClick = (item, index) => {
     console.log("click me")
@@ -95,11 +105,33 @@ export default class Index extends Component {
             ]}
           />
         </View>
-        {/* <View className='infoList'>
-          <View className="LeftImg">left</View>
-          <View className="RightInfoItem">right</View>
-        </View> */}
-
+        <AtAccordion
+          title="标题三"
+          icon={{ value: "share", color: "red", size: "15" }}
+          open={this.state.open}
+          onClick={this.handleClickAAA.bind(this)}
+        >
+          <AtList hasBorder={false}>
+            <AtListItem
+              title="标题文字"
+              arrow="right"
+              thumb="https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png"
+            />
+            <AtListItem
+              title="标题文字"
+              note="描述信息"
+              arrow="right"
+              thumb="http://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png"
+            />
+            <AtListItem
+              title="标题文字"
+              note="描述信息"
+              extraText="详细信息"
+              arrow="right"
+              thumb="http://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png"
+            />
+          </AtList>
+        </AtAccordion>
       </View>
     )
   }
